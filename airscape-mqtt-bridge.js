895f4a65cdb557a2fc12a605f8d3fe6b49c78342
Justin Wood <woody@apple.com>
2018-09-03 21:45:04 -0700

    Updating airscape

--- airscape-mqtt-bridge.js
+++ airscape-mqtt-bridge.js
@@ -1,7 +1,7 @@
 // Requirements
 const mqtt = require('mqtt')
 const logging = require('homeautomation-js-lib/logging.js')
-const airscape = require('homeautomation-js-lib/airscape.js')
+const airscape = require('./lib/airscape.js')
 const _ = require('lodash')
 const health = require('homeautomation-js-lib/health.js')
 
@@ -10,13 +10,13 @@ require('homeautomation-js-lib/mqtt_helpers.js')
 var shouldRetain = process.env.MQTT_RETAIN
 
 if (_.isNil(shouldRetain)) {
-    shouldRetain = true
+	shouldRetain = true
 }
 
 var mqttOptions = {}
 
 if (!_.isNil(shouldRetain)) {
-    mqttOptions['retain'] = shouldRetain
+	mqttOptions['retain'] = shouldRetain
 }
 
 // Config
@@ -24,61 +24,64 @@ const set_string = '/set'
 const airscapeTopic = process.env.AIRSCAPE_TOPIC
 
 if (_.isNil(airscapeTopic)) {
-    logging.warn('AIRSCAPE_TOPIC not set, not starting')
-    process.abort()
+	logging.warn('AIRSCAPE_TOPIC not set, not starting')
+	process.abort()
 }
 
 var connectedEvent = function() {
-    client.subscribe(airscapeTopic + set_string)
-    health.healthyEvent()
+	client.subscribe(airscapeTopic + set_string)
+	health.healthyEvent()
 }
 
 var disconnectedEvent = function() {
-    health.unhealthyEvent()
+	health.unhealthyEvent()
 }
 
 // Setup MQTT
 var client = mqtt.setupClient(connectedEvent, disconnectedEvent)
 
 client.on('message', (topic, message) => {
-    if (_.isNil(message) || _.isNil(topic))
-        return
+	if (_.isNil(message) || _.isNil(topic)) {
+		return
+	}
 
-    airscape.setSpeed(parseInt(message))
-    health.healthyEvent()
+	airscape.setSpeed(parseInt(message))
+	health.healthyEvent()
 })
 
 airscape.on('fan-updated', (result) => {
-    if (_.isNil(result)) {
-        logging.error('Airscape update failed')
-        health.unhealthyEvent()
-        return
-    }
+	if (_.isNil(result)) {
+		logging.error('Airscape update failed')
+		health.unhealthyEvent()
+		return
+	}
 
-    const changedKeys = Object.keys(result)
-    logging.info('Airscape updated: ' + changedKeys)
+	const changedKeys = Object.keys(result)
+	logging.info('Airscape updated: ' + changedKeys)
 
-    if (_.isNil(changedKeys)) {
-        health.unhealthyEvent()
-    } else {
-        health.healthyEvent()
-    }
+	if (_.isNil(changedKeys)) {
+		health.unhealthyEvent()
+	} else {
+		health.healthyEvent()
+	}
 
-    changedKeys.forEach(
-        function(this_key) {
-            if (this_key === 'server_response')
-                return
+	changedKeys.forEach(
+		function(this_key) {
+			if (this_key === 'server_response') { 
+				return 
+			}
 
-            const value = result[this_key]
+			const value = result[this_key]
 
-            if (_.isNil(value))
-                return
+			if (_.isNil(value)) {
+				return
+			}
 
-            if (this_key === 'fanspd') {
-                client.smartPublish(airscapeTopic, '' + value, mqttOptions)
-            } else {
-                client.smartPublish(airscapeTopic + '/' + this_key, '' + value, mqttOptions)
-            }
-        }
-    )
+			if (this_key === 'fanspd') {
+				client.smartPublish(airscapeTopic, '' + value, mqttOptions)
+			} else {
+				client.smartPublish(airscapeTopic + '/' + this_key, '' + value, mqttOptions)
+			}
+		}
+	)
 })
